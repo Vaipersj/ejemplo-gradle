8f21944751b7e3b03956abb7e2031ca17c85a695
CBISSOTTO <vaipersj@gmail.com>
2022-02-03 14:39:33 -0300

    prueba nombre del BRANCH 2

--- gradle.groovy
+++ gradle.groovy
@@ -12,8 +12,7 @@ def call(){
         sh "echo 'Análisis Estático!'"
         withSonarQubeEnv('sonarqube') {
             sh './gradlew sonarqube -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build'
-            sh '[${env.JOB_NAME}] '
-        }
+            sh "[${env.BRANCH_NAME}]"
     } 
     stage("Paso 3: Curl Springboot Gradle sleep 20"){
         sh "gradle bootRun&"
